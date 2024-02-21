d2b59b37fce2d10e9a11326f51b13ef06f91292e
Vinusha-V <90742897+Vinusha-V@users.noreply.github.com>
2024-02-21 10:03:00 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    BASE_DIR / "static",
+    'var/www/static',
+    'staticfiles',
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os 
